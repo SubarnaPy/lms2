dab8669f194607bab281fc65194e9c6d983d3e53
Subarna Mondal <mondalsubarna29@gmail.com>
2025-02-20 05:09:24 +0530

    Update React version to 19

--- frontendd/vite.config.js
+++ frontendd/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['lodash', 'axios'],
+          chartjs: ['chart.js'],
+          mui: ['@mui/material', '@mui/icons-material'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the limit if needed
+  },
+});

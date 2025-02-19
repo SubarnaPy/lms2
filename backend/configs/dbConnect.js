0f63c01a1f966f607bca8932616e604a98571bec
Subarna Mondal <mondalsubarna29@gmail.com>
2025-02-20 04:14:27 +0530

    ready to start

---
+++ backend/configs/dbConnect.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+config();
+import{config} from 'dotenv';
+
+
+mongoose.set('strictQuery',false);
+
+
+const connectToDb=async()=>{
+    try{
+        console.log('connect',process.env.MONGO_URI2);
+
+        const {connection}=await mongoose.connect('mongodb+srv://mondalsubarna29:Su12345@cluster0.1kmazke.mongodb.net/LMSS');
+
+
+        if(connection){
+            console.log('Connecting to mongodb: ${connection.host}');
+        }
+    } catch(e){
+        console.log(e);
+        process.exit(1);
+    }
+};
+
+
+export default connectToDb;

20c243597802a6452a1108633171a22e26f05ef7
Subarna Mondal <mondalsubarna29@gmail.com>
2025-02-20 07:06:42 +0530

    Fix build issue

--- frontendd/vite.config.js
+++ frontendd/vite.config.js
@@ -3,17 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          vendor: ['lodash', 'axios'],
-          chartjs: ['chart.js'],
-          mui: ['@mui/material', '@mui/icons-material'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Adjust the limit if needed
-  },
 });
